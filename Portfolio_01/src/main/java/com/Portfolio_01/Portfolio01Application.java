4bca1c7bc45e452bee0f283a6ceae8b55c60e43a
StromKang <maxsore0425@gmail.com>
2021-11-15 20:49:25 +0900

    Start

---
+++ Portfolio_01/src/main/java/com/Portfolio_01/Portfolio01Application.java
@@ -0,0 +1,13 @@
+package com.Portfolio_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portfolio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portfolio01Application.class, args);
+	}
+
+}
---
+++ Portfolio_01/src/test/java/com/Portfolio_01/Portfolio01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portfolio01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
